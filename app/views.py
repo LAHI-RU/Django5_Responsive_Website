e33a40188d496162bc8c3b58093822559f7a8f22
LAHI-RU <lahiiru.dananjaya@gmail.com>
2024-12-12 10:36:20 +0530

    update

--- app/views.py
+++ app/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html", {})
+    context = {
+        'course_title': 'Django Course',
+        'current_date': '2024-12-12',
+        'user': {
+            'name': 'John Doe',
+            'email': 'john@example.com',
+        },
+        'product_price': 199.99999,
+        'random_text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    }
+    return render(request, "index.html", context)

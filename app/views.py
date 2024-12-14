87c967edb67e2b69ee8a1385656cbb607d7787c8
LAHI-RU <lahiiru.dananjaya@gmail.com>
2024-12-14 18:58:48 +0530

    update

--- app/views.py
+++ app/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
+from app.models import GeneralInfo
+
+# from django.db import connection
+# def write_sql_queries_to_file(file_path):
+#     with open(file_path, 'w') as file:
+#         queries = connection.queries
+#         for query in queries:
+#             sql = query['sql']
+#             file.write(f"{sql}\n")
 
 
-# Create your views here.
+# # Create your views here.
 def index(request):
+
+#     # ORM  - Object-Relational Mapping
+
+#     # ORM is a programming technique that allows developers to interact
+#     # with database using Python objects instead of raw SQL queries
+
+#     # READ
+#     all_records = GeneralInfo.objects.all()
+#     print(all_records)
+#     # DELETE
+#     # UPDATE
+#     # CREATE
+
+#     file_path = 'sql_queries.txt'
+#     write_sql_queries_to_file(file_path)
+
+    general_info = GeneralInfo.objects.first()
+    print(f"general_info:  {general_info.location}")
+
     context = {}
     return render(request, "index.html", context)

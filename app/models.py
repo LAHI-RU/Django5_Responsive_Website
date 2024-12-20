183e2a7b0f654ed54a44b7614176ac11f0a337c7
LAHI-RU <lahiiru.dananjaya@gmail.com>
2024-12-20 10:16:50 +0530

    update

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from ckeditor.fields import RichTextField
 
 class GeneralInfo(models.Model):
     company_name = models.CharField(max_length=255, default="Company")
@@ -79,7 +80,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.PROTECT, null=True, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
-    content = models.TextField()
+    content = RichTextField()  # models.TextField()
 
     def __str__(self):
         return self.title
